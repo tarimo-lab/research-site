a1a3071ebc5d4fafa7d80bc87dd93938e5d8c1e8
jmwai80 <57574038+jmwai80@users.noreply.github.com>
2021-05-28 21:15:07 -0400

    Delete blog directory

--- blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from blog.models import Profile
-
-def index(request):
-    profiles = Profile.objects.all()
-    return render(request, 'blog/index.html', {'profiles': profiles})
