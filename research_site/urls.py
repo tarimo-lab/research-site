1a7277d261a7b254707b450d7150ab06615fa2c2
John Mwai <jmwai@conncol.edu>
2021-06-14 00:19:53 -0400

    added create pages

--- research_site/urls.py
+++ research_site/urls.py
@@ -1,22 +1,27 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
-from .views import (HomePage, AboutPage, ProjectCategoryView, ProjectCategoryCreateView, ProjectView, ProjectDetailView, PublicationView, PublicationDetailView, BlogView, BlogSingle)
+from .views import (HomePage, AboutPage, ProfileCreateView, ProjectCategoryView, CategoryDetailView, ProjectCategoryCreateView, ProjectView, ProjectCreateView, ProjectDetailView, PublicationView, PublicationDetailView,PublicationCreateView, BlogView, BlogSingle, BlogCreateView)
 from . import views
 
 urlpatterns = [
 	path('', HomePage.as_view(), name='home'),
 	path('about/', AboutPage.as_view(), name='about'),
+	path('create/profile/', ProfileCreateView.as_view(), name='profile_create'),
 	path('categories/', ProjectCategoryView.as_view(), name='categories'),
 	path('categories/<int:pk>/', views.categoryDetail, name='category_detail'),
+	#path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
 	path('projects/', ProjectView.as_view(), name='projects'),
 	path('projects/<str:slug>/', ProjectDetailView.as_view(), name='project_detail'),
-    path('create/project_category/', ProjectCategoryCreateView.as_view(),
-     name='project_category_create'),
+	path('create/project/', ProjectCreateView.as_view(), name='project_create'),
+    path('create/project_category/', ProjectCategoryCreateView.as_view(), name='project_category_create'),
 	path('blogs/', BlogView.as_view(), name='blogs'),
 	path('blogs/<str:slug>/', BlogSingle.as_view(), name='blog_detail'),
+	path('create/blog/', BlogCreateView.as_view(), name='blog_create'),
 	path('publications/', PublicationView.as_view(), name='publications'),
 	path('publications/<int:pk>/', PublicationDetailView.as_view(), name='publication_detail'),
+	path('create/publication/', PublicationCreateView.as_view(), name='publication_create'),
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- research_site/views.py
+++ research_site/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import generic, View
 from django.db.models import Q
 from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Profile, Post, Publication, Project, ProjectCategory, Gallery
+from django.shortcuts import render, redirect, get_object_or_404
 
 """ HomePage """
 class HomePage(TemplateView):
@@ -26,9 +26,19 @@ class AboutPage(TemplateView):
     def get(self, request, *args, **kwargs):
         context = {
             'profiles': Profile.objects.order_by('-pk'),
+            'images': Gallery.objects.order_by('-pk'),
         }
         return render(request, self.template_name, context)
 
+class ProfileCreateView(LoginRequiredMixin, CreateView):
+    model = Profile
+    template_name = 'create/new_profile.html'
+    fields = "__all__"
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 """ Projects Category"""
 class ProjectCategoryView(ListView):
     model = ProjectCategory
@@ -50,6 +60,9 @@ class ProjectCategoryCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class CategoryDetailView(DetailView):
+    model = Publication
+    template_name = "category_detail.html"
 """ Projects """
 class ProjectView(ListView):
     model = Project
@@ -60,6 +73,17 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = "project_detail.html"
 
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = 'create/new_project.html'
+    fields = "__all__"
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 """ Publications """
 class PublicationView(ListView):
     model = Publication
@@ -69,6 +93,16 @@ class PublicationView(ListView):
 class PublicationDetailView(DetailView):
     model = Publication
     template_name = "publication_detail.html"
+
+class PublicationCreateView(LoginRequiredMixin, CreateView):
+    model = Publication
+    template_name = 'create/new_publication.html'
+    fields = "__all__"
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 """ Blog """
@@ -84,4 +118,13 @@ class BlogSingle(generic.DetailView):
     def get_queryset(self):
         return self.model.objects.filter(slug=self.kwargs['slug'])
 
- 
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'create/new_blog.html'
+    fields = "__all__"
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
